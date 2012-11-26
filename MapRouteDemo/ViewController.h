12ecd137ed70bf769509f23f0d97af3a994e2c89
kai <kai.901025@gmail.com>
2012-11-26 10:28:41 +0800

    Initial Commit

---
+++ MapRouteDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapRouteDemo
+//
+//  Created by 5dscape on 12-11-26.
+//  Copyright (c) 2012年 kai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MapRouteDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapRouteDemo
+//
+//  Created by 5dscape on 12-11-26.
+//  Copyright (c) 2012年 kai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
